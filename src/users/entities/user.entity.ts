e687e859a018a0508dda5897eac8dbf8a0506794
Nathan1Waxin <nathan.waxin@etu.unilim.fr>
2023-01-22 20:39:25 +0100

    remplacmeent du code type ORM en type mongodb

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,28 +1,6 @@
-import { Entity, Column, PrimaryGeneratedColumn, EntityRepository, Repository } from 'typeorm';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
-@Entity()
-export class Users {
-  @PrimaryGeneratedColumn()
-  id: number;
 
-  @Column()
-  firstname: string;
 
-  @Column()
-  lastname: string;
-
-  @Column({ unique: true })
-  public email: string;
- 
-  @Column()
-  public password: string;
-
-  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP"})
-  createdAt: Date;
-
-  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP"})
-  updatedAt: Date;
-}
-
-@EntityRepository(Users)
-export class UsersRepository extends Repository<Users> {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
